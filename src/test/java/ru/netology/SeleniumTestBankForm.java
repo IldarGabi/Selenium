e87655ff9a6dfaccbd1bead02f01dc5c5c3e9088
IldarGabi <IldarGabi@gmail.com>
2021-02-09 00:13:25 +0300

    Optimization code, add test, delete comments

--- src/test/java/ru/netology/SeleniumTestBankForm.java
+++ src/test/java/ru/netology/SeleniumTestBankForm.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -19,7 +18,6 @@ public class SeleniumTestBankForm {
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\QA igabi\\Projects\\Autotesting\\Selenium\\driver\\chromedriver.exe");
     }
 
     @BeforeEach
@@ -29,6 +27,7 @@ public class SeleniumTestBankForm {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -38,8 +37,7 @@ public class SeleniumTestBankForm {
     }
 
     @Test
-    void cardOrderForm(){
-        driver.get("http://localhost:9999/");
+    void cardOrderForm() {
         driver.findElement((By.cssSelector("[type=text]"))).sendKeys("Ильдар");
         driver.findElement((By.cssSelector("[type=tel]"))).sendKeys("+79784440001");
         driver.findElement((By.cssSelector(".checkbox__box"))).click();
@@ -47,9 +45,9 @@ public class SeleniumTestBankForm {
         String checkText = driver.findElement(By.cssSelector(".Success_successBlock__2L3Cw")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", checkText.trim());
     }
+
     @Test
-    void cardOrderFormIfNoName(){
-        driver.get("http://localhost:9999/");
+    void cardOrderFormIfNoName() {
         driver.findElement((By.cssSelector("[type=text]"))).sendKeys("");
         driver.findElement((By.cssSelector("[type=tel]"))).sendKeys("+79784440001");
         driver.findElement((By.cssSelector(".checkbox__box"))).click();
@@ -59,8 +57,7 @@ public class SeleniumTestBankForm {
     }
 
     @Test
-    void cardOrderFormIfNoTel(){
-        driver.get("http://localhost:9999/");
+    void cardOrderFormIfNoTel() {
         driver.findElement((By.cssSelector("[type=text]"))).sendKeys("Ильдар");
         driver.findElement((By.cssSelector("[type=tel]"))).sendKeys("");
         driver.findElement((By.cssSelector(".checkbox__box"))).click();
@@ -70,8 +67,7 @@ public class SeleniumTestBankForm {
     }
 
     @Test
-    void cardOrderFormIfNotValidName(){
-        driver.get("http://localhost:9999/");
+    void cardOrderFormIfNotValidName() {
         driver.findElement((By.cssSelector("[type=text]"))).sendKeys("znqa-11");
         driver.findElement((By.cssSelector("[type=tel]"))).sendKeys("+79784440001");
         driver.findElement((By.cssSelector(".checkbox__box"))).click();
@@ -81,14 +77,22 @@ public class SeleniumTestBankForm {
     }
 
     @Test
-    void cardOrderFormIfShortTel(){
-        driver.get("http://localhost:9999/");
+    void cardOrderFormIfShortTel() {
         driver.findElement((By.cssSelector("[type=text]"))).sendKeys("Ильдар");
         driver.findElement((By.cssSelector("[type=tel]"))).sendKeys("+7978");
         driver.findElement((By.cssSelector(".checkbox__box"))).click();
         driver.findElement((By.cssSelector(".button__text"))).submit();
         String checkText = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", checkText);
+    }
+
+    @Test
+    void cardOrderFormIfNotClickCheckBox() {
+        driver.findElement((By.cssSelector("[type=text]"))).sendKeys("Ильдар");
+        driver.findElement((By.cssSelector("[type=tel]"))).sendKeys("+79784440001");
+        driver.findElement((By.cssSelector(".button__text"))).submit();
+        String checkText = driver.findElement(By.cssSelector(".checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", checkText);
     }
 }
 
